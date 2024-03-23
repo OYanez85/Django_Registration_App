b0f9664eb78ea4af60f04722088127f69fd00742
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-03-23 22:49:40 +0100

    Django_Reg_App

---
+++ website/register/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+# Create your views here.
+
+def index(request):
+    return HttpResponse("View: registration index")
+
+from .forms import RegForm
+
+def reg_view(request):
+    context = {}
+    context['form']=RegForm()
+    return render(request, "reg.html", context)
